e2b9a90e97aa787ad4d6e6e0401f3a74efa2c4a8
dscobol <dscobol@gmail.com>
2023-03-15 20:32:25 -0400

    Convert UPDTCUST from GETDBID to ACCEPT FROM ENVIRONMENT.

--- jcl/updtcust-run.sh
+++ jcl/updtcust-run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Set Up
+# Set Up variables
+
+# Program to run
 PGM=UPDTCUST
 export DD_VFILE1="../idata/customrs.idat"
 export DD_VFILE2="../idata/errcustmrs.idat"
+
+# Export the environment variables in the .env file
+export $(grep -v '^#' ../.env | xargs)
 
 # run program
 ../bin/$PGM
