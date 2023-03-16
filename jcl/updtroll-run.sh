9e32f086ec4953a5c738309e5c51962c505fe498
dscobol <dscobol@gmail.com>
2023-03-15 20:32:57 -0400

    Convert UPDTROLL from GETDBID to ACCEPT FROM ENVIRONMENT.

--- jcl/updtroll-run.sh
+++ jcl/updtroll-run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Set Up
-PGM=UPDTCUST
+# Set Up variables
+
+# Program to run
+PGM=UPDTROLL
 export DD_VFILE1="../idata/customrs.idat"
 export DD_VFILE2="../idata/errcustmrs2.idat"
+
+# Export the environment variables in the .env file
+export $(grep -v '^#' ../.env | xargs)
 
 # run program
 ../bin/$PGM
