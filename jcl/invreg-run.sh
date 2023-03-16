0eab62aabe3dbada4d2843ccecd4bffc6e028a5a
dscobol <dscobol@gmail.com>
2023-03-15 20:30:55 -0400

    Convert INVREG from GETDBID to ACCEPT FROM ENVIRONMENT.

--- jcl/invreg-run.sh
+++ jcl/invreg-run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Set Up
+# Set Up variables
+
+# Program to run
 PGM=INVREG
 export DD_RINVREG="../spool/invoice.rpt"
+
+# Delete previous versions
+rm $DD_RINVREG
+
+# Export the environment variables in the .env file
+export $(grep -v '^#' ../.env | xargs)
 
 # run program
 ../bin/$PGM
