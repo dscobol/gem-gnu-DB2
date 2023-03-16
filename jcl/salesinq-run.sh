ee9439a774f4040d2ee5b2ef54b6fe52076b977c
dscobol <dscobol@gmail.com>
2023-03-15 20:31:47 -0400

    Convert SALESINQ from GETDBID to ACCEPT FROM ENVIRONMENT.

--- jcl/salesinq-run.sh
+++ jcl/salesinq-run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Set Up
+# Set Up variables
+
+# Program to run
 PGM=SALESINQ
+
+# Export the environment variables in the .env file
+export $(grep -v '^#' ../.env | xargs)
 
 # run program
 ../bin/$PGM
